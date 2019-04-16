e197f3ee530c28e08444d52ea2c936e2a9669645
Julien LEVIEIL <julien@levieil.fr>
2019-04-17 09:25:06 +1100

    add mpc mpd ncmppc and i3 config for mpc

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-sudo apt-get install dfc htop vim i3-wm i3status dmenu rxvt-unicode-256color zsh curl wget git mpv golang-go
+sudo apt-get -y install dfc htop vim i3-wm i3status dmenu rxvt-unicode-256color zsh curl wget git mpv golang-go ncmpcpp mpd mpc
 
 # NEOVIM
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt-get -y update
+sudo apt-get -y install neovim
+sudo apt-get -y install python-dev python-pip python3-dev python3-pip
 pip3 install --user --upgrade pynvim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
